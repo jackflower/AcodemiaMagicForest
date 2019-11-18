eb1f0592ba7c048cb8e0c1f60d14f61773d42e6f
jackflower <jackflower@poczta.onet.pl>
2019-11-18 13:55:39 +0100

    Update
    Aktualizacja repozytorium - do publikacji

---
+++ mine_one/MineExplosionOne.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+# 2019-11-23 acodemia.pl
+
+var damage = 0
+
+func _ready():
+	pass
+	
+	
+func _on_AnimationPlayer_animation_finished( anim_name ):
+	queue_free()
+	get_parent().queue_free()
+	pass
+	
+	
+func _on_MineExplosion_body_entered( body ):
+	if(body.has_method("update_health")):
+		body.update_health(int(damage))
+	get_node("CollisionShape2D").disabled = true
+	pass
+	
