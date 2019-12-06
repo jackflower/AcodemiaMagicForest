c50fb85379a6f45d74b55b0c4f3ee83231eb1ef4
jackflower <jackflower@poczta.onet.pl>
2019-12-06 15:38:46 +0100

    Aktualizacja sceny
    Dekoracja - roślinność, eliksiry, miny, szkielety, wieże. Ustawienie stopnia trudności poszczególnych skąłdowych na scenie.

--- main.gd
+++ main.gd
@@ -11,6 +11,13 @@ func _ready():
 	$Tower_01.setTarget($Wanderer)
 	$Tower_02.setTarget($Wanderer)
 	$Tower_03.setTarget($Wanderer)
+	$Tower_04.setTarget($Wanderer)
+	$Tower_05.setTarget($Wanderer)
+	$Tower_06.setTarget($Wanderer)
+	$Tower_07.setTarget($Wanderer)
+	$Tower_08.setTarget($Wanderer)
+	$Tower_09.setTarget($Wanderer)
+	$Tower_10.setTarget($Wanderer)
 	pass
 
 #func _process(delta):
